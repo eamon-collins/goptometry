8b2b94fbae05cbd92a33508fd2fadf3680504178
eamon-collins <eamonacollins@gmail.com>
2017-06-21 18:29:46 -0400

    UI and adding other models

--- main.go
+++ main.go
@@ -36,6 +36,7 @@ type Tag struct {
 }
 
 type Results struct {
+	Clarifai  Company
 	Companies []Company
 }
 
@@ -50,9 +51,15 @@ func main() {
 }
 
 func index(w http.ResponseWriter, r *http.Request) {
+	funcMap := template.FuncMap{
+		"url": func(s string) template.URL {
+			return template.URL(s)
+		},
+	}
 	ip := filepath.Join("templates", "index.html")
-	tp := filepath.Join("templates", "template.html")
-	tmpl, err := template.ParseFiles(ip, tp)
+	fp := filepath.Join("templates", "form.html")
+  rp := filepath.Join("templates", "response.html")
+	tmpl, err := template.New("template").Funcs(funcMap).ParseFiles(ip, fp, rp)
 	if err != nil {
 		fmt.Println(err)
 		panic(err)
@@ -62,11 +69,30 @@ func index(w http.ResponseWriter, r *http.Request) {
 		tmpl.ExecuteTemplate(w, "index", nil)
 	} else {
 		r.ParseForm()
+
+		//don"t make requests to any apis, just give prefetched data in format to test out the layout
+		if r.FormValue("layouttest") == "layouttest" {
+			var results Results
+			results.Clarifai = Company{Company: "Clarifai", Elapsed: 1.322, Tags: []Tag{Tag{Label: "summer", Score: 0.98168695}, Tag{Label: "nature", Score: 0.97212166}, Tag{Label: "farm", Score: 0.9689047}, Tag{Label: "grass", Score: 0.95977676}, Tag{Label: "outdoors", Score: 0.9521229}, Tag{Label: "field", Score: 0.9091958}}}
+			results.Companies = append(results.Companies, Company{Company: "Google", Elapsed: 2.332, Tags: []Tag{Tag{Label: "rural area", Score: 0.6766008}, Tag{Label: "farm", Score: 0.6482424}, Tag{Label: "meadow", Score: 0.59396565}, Tag{Label: "horse like mammal", Score: 0.51159537}}})
+			results.Companies = append(results.Companies, Company{Company: "IBM", Elapsed: 2.109, Tags: []Tag{Tag{Label: "green color", Score: 0.963}, Tag{Label: "animal", Score: 0.765}}}) /** {"tag": u"mammal", "score": 0.653}, {"tag": u"domestic animal", "score": 0.653}, {"tag": u"dog", "score": 0.652}, {"tag": u"ruminant", "score": 0.603}, {"tag": u"deer", "score": 0.602}, {"tag": u"vizsla dog", "score": 0.569}, {"tag": u"Great Dane dog", "score": 0.558}, {"tag": u"person", "score": 0.55}, {"tag": u"boy at farm", "score": 0.549}, {"tag": u"palomino horse", "score": 0.53}]})
+			  if amazon:
+			    results.append({"company": "Amazon", "elapsed": 3.217, "tags": [{"tag": u"Human", "score": 99.30406951904297}, {"tag": u"People", "score": 99.30635070800781}, {"tag": u"Person", "score": 99.30635070800781}, {"tag": u"Backyard", "score": 77.3866958618164}, {"tag": u"Yard", "score": 77.3866958618164}, {"tag": u"Ivy", "score": 76.58114624023438}, {"tag": u"Plant", "score": 76.58114624023438}, {"tag": u"Vine", "score": 76.58114624023438}, {"tag": u"Shorts", "score": 75.4872055053711}, {"tag": u"Blossom", "score": 67.45735168457031}, {"tag": u"Flora", "score": 67.45735168457031}, {"tag": u"Flower", "score": 67.45735168457031}, {"tag": u"Herbal", "score": 63.904659271240234}, {"tag": u"Herbs", "score": 63.904659271240234}, {"tag": u"Planter", "score": 63.904659271240234}]})
+			  if microsoft:
+			    results.append({"company": "Microsoft", "elapsed": 2.698, "tags": [{"tag": u"tree", "score": 0.9998617172241211}, {"tag": u"outdoor", "score": 0.999527096748352}, {"tag": u"grass", "score": 0.9965176582336426}, {"tag": u"standing", "score": 0.8547968864440918}, {"tag": u"house", "score": 0.4055963158607483}]})
+			  **/
+			tmpl.ExecuteTemplate(w, "index", &results)
+			return
+		}
+
 		imgurl := template.HTMLEscapeString(r.Form.Get("imgurl"))
 		comp_map := make(map[string]bool)
 		for _, comp := range r.Form["competitors"] {
 			comp_map[comp] = true
-	  }
+		}
+		model_id := r.Form.Get("model_id")
+    fmt.Println(model_id)
+
 		//make a base64 encoding of the image at the imgurl
 		//amazon just wants normal bytes, but someone else might want base64
 		image_res, err := http.Get(imgurl)
@@ -83,62 +109,125 @@ func index(w http.ResponseWriter, r *http.Request) {
 
 		//the results object to write the responses to as they are passed to the channel
 		//will be passed to the html once filled
-    var results Results
+		var results Results
 
 		res_channel := make(chan *Company)
 
-    for key, _ := range comp_map {
-      go func(imgurl string, image_bytes []byte, key string) {
-  		  //CLARIFAI CLIENT PREDICT
-    		if key == "Clarifai"{
-          res_channel <- client_clarifai(imgurl)
-    		}
-    		//GOOGLE CLOUD VISION
-    		if key == "Google" {
-    			res_channel <- request_google(imgurl)
-    		}
-    		//MICROSOFT AZURE VISUAL RECOGNITION
-    		if key == "Microsoft" {
-    			res_channel <- request_microsoft(imgurl)
-    		}
-    		//AMAZON REKOGNITION
-    		if key == "Amazon" {
-    			res_channel <- client_amazon(image_bytes)
-    		}
-      }(imgurl, image_bytes, key)
-    }
+    //For each company result requested, start a function in a separate goroutine to retrieve that data
+    //This allows all requests to be executed concurently rather than sequentially
+    go func(imgurl string, model_id string) {
+      res_channel <- request_clarifai(imgurl, model_id)
+    }(imgurl, model_id)
 
-    //waits for responses from the goroutines fetching the requested companies,
-    //adds them to results.Companies as they come in.
-    for {
-      select {
-      case r := <-res_channel:
-        results.Companies = append(results.Companies, *r)
-        if len(results.Companies) == len(comp_map){
-          //return html, later may do this each time a company is received to simulate
-          //ajax calls, except it would reload whole result every time.
-          tmpl.ExecuteTemplate(w, "index", &results)
-          return
-        }
-      }
-    }
+		for key, _ := range comp_map {
+			go func(imgurl string, image_bytes []byte, key string) {
+				//GOOGLE CLOUD VISION
+				if key == "Google" {
+					res_channel <- request_google(imgurl)
+				}
+				//MICROSOFT AZURE VISUAL RECOGNITION
+				if key == "Microsoft" {
+					res_channel <- request_microsoft(imgurl)
+				}
+				//AMAZON REKOGNITION
+				if key == "Amazon" {
+					res_channel <- client_amazon(image_bytes)
+				}
+				//IBM VISUAL RECOGNITION
+				if key == "IBM" {
+					res_channel <- request_ibm(imgurl)
+				}
+			}(imgurl, image_bytes, key)
+		}
+
+		//waits for responses from the goroutines fetching the requested companies,
+		//adds them to results.Companies as they come in.
+		//tmpl.ExecuteTemplate(w, "index", nil)
+		for {
+			select {
+			case r := <-res_channel:
+				res := *r
+				if res.Company == "Clarifai" {
+					results.Clarifai = res
+				} else {
+					results.Companies = append(results.Companies, res)
+				}
+				if (len(results.Companies) == len(comp_map)) && (results.Clarifai.Company == "Clarifai") {
+					//return html, later may do this each time a company is received to simulate
+					//ajax calls, except it would reload whole result every time.
+					tmpl.ExecuteTemplate(w, "index", &results)
+					return
+			  }
+      case <-time.After(time.Second * 5):
+        fmt.Println("Timeout")
+        tmpl.ExecuteTemplate(w, "index", &results)
+        return
+			}
+		}
 	}
 
 }
 
 func client_clarifai(imgurl string) *Company {
-  clarifai_predict := clarifai.NewClient(secrets.Clarifai_Client_ID, secrets.Clarifai_Client_Secret)
-  start := time.Now()
-  clarifai_resp, err := clarifai_predict.Tag(clarifai.TagRequest{URLs: []string{imgurl}})
-  elapsed := time.Since(start)
-  if err != nil {
-    fmt.Println(err)
-  }
-  c := Company{Company: "Clarifai", Elapsed: elapsed.Seconds()}
-  for i, label := range clarifai_resp.Results[0].Result.Tag.Classes {
-    c.Tags = append(c.Tags, Tag{label, clarifai_resp.Results[0].Result.Tag.Probs[i]})
-  }
-  return &c
+	clarifai_predict := clarifai.NewClient(secrets.Clarifai_Client_ID, secrets.Clarifai_Client_Secret)
+	start := time.Now()
+	clarifai_resp, err := clarifai_predict.Tag(clarifai.TagRequest{URLs: []string{imgurl}})
+	elapsed := time.Since(start)
+	if err != nil {
+		fmt.Println(err)
+	}
+	c := Company{Company: "Clarifai", Elapsed: elapsed.Seconds()}
+	for i, label := range clarifai_resp.Results[0].Result.Tag.Classes {
+		c.Tags = append(c.Tags, Tag{label, clarifai_resp.Results[0].Result.Tag.Probs[i]})
+	}
+	return &c
+}
+
+func request_clarifai(imgurl string, model_id string) *Company {
+	//response structure
+	type ClarifaiJson struct {
+		Outputs []struct {
+			Data struct {
+				Concepts []struct {
+					Label string  `json:"name"`
+					Score float32 `json:"value"`
+				} `json:"concepts"`
+			} `json:"data"`
+		} `json:"outputs"`
+	}
+
+	//build the request
+	client := &http.Client{
+		Timeout: time.Second * 10,
+	}
+	body := []byte(`{"inputs": [ { "data": {"image": { "url":"` + imgurl + `"}}}]}`)
+	req, err := http.NewRequest("POST", "https://api.clarifai.com/v2/models/"+model_id+"/outputs", bytes.NewBuffer(body))
+
+	req.Header.Set("Authorization", "Bearer "+secrets.Clarifai_Token)
+	req.Header.Set("Content-Type", "application/json")
+
+	//make the request
+	start := time.Now()
+	resp, err := client.Do(req)
+	elapsed := time.Since(start)
+	if err != nil {
+		panic(err)
+	}
+
+	//parse the response
+	defer resp.Body.Close()
+	data, _ := ioutil.ReadAll(resp.Body)
+	var dat ClarifaiJson
+	if err := json.Unmarshal(data, &dat); err != nil {
+		panic(err)
+	}
+	c := Company{Company: "Clarifai", Elapsed: elapsed.Seconds()}
+	for _, tag := range dat.Outputs[0].Data.Concepts {
+		c.Tags = append(c.Tags, Tag{tag.Label, tag.Score})
+	}
+
+	return &c
+
 }
 
 func request_google(imgurl string) *Company {
@@ -239,7 +328,7 @@ func request_microsoft(imgurl string) *Company {
 
 func client_amazon(b64image []byte) *Company {
 	//response structure
-	//as long as I'm using the client to make the request, don't strictly need this but
+	//as long as I"m using the client to make the request, don"t strictly need this but
 	//good to have it around as a template for how the response is structured
 	type AmazonJson struct {
 		Labels []struct {
@@ -264,53 +353,53 @@ func client_amazon(b64image []byte) *Company {
 	for _, tag := range resp.Labels {
 		a.Tags = append(a.Tags, Tag{*tag.Name, float32(*tag.Confidence)})
 	}
-	fmt.Println(a)
+
 	return &a
 }
 
 func request_ibm(imgurl string) *Company {
-  //response structure
-  type IbmJson struct {
-    Images []struct {
-      Classifiers []struct{
-        Tags []struct {
-          Label string  `json:"class"`
-          Score float32 `json:"score"`
-        } `json:"classes"`
-      } `json:"classifiers"`
-    } `json:"images"`
-  }
+	//response structure
+	type IbmJson struct {
+		Images []struct {
+			Classifiers []struct {
+				Tags []struct {
+					Label string  `json:"class"`
+					Score float32 `json:"score"`
+				} `json:"classes"`
+			} `json:"classifiers"`
+		} `json:"images"`
+	}
 
-  //build the request
-  client := &http.Client{
-    Timeout: time.Second * 10,
-  }
-  body := []byte(`{"url":"` + imgurl + `"}`)
-  req, err := http.NewRequest("POST", "https://gateway-a.watsonplatform.net/visual-recognition/api/v3/classify", bytes.NewBuffer(body))
-  params := req.URL.Query()
-  params.Add("api_key", secrets.Ibm_Api_Key)
-  params.Add("version", "2016-05-20")
-  req.URL.RawQuery = params.Encode()
+	//build the request
+	client := &http.Client{
+		Timeout: time.Second * 10,
+	}
+	body := []byte(`{"url":"` + imgurl + `"}`)
+	req, err := http.NewRequest("POST", "https://gateway-a.watsonplatform.net/visual-recognition/api/v3/classify", bytes.NewBuffer(body))
+	params := req.URL.Query()
+	params.Add("api_key", secrets.Ibm_Api_Key)
+	params.Add("version", "2016-05-20")
+	req.URL.RawQuery = params.Encode()
 
-  //make the request
-  start := time.Now()
-  resp, err := client.Do(req)
-  elapsed := time.Since(start)
-  if err != nil {
-    panic(err)
-  }
+	//make the request
+	start := time.Now()
+	resp, err := client.Do(req)
+	elapsed := time.Since(start)
+	if err != nil {
+		panic(err)
+	}
 
-  //parse the response
-  defer resp.Body.Close()
-  data, _ := ioutil.ReadAll(resp.Body)
-  var dat IbmJson
-  if err := json.Unmarshal(data, &dat); err != nil {
-    panic(err)
-  }
-  i := Company{Company: "IBM", Elapsed: elapsed.Seconds()}
-  for _, tag := range dat.Tags {
-    m.Tags = append(m.Tags, Tag{tag.Label, tag.Score})
-  }
+	//parse the response
+	defer resp.Body.Close()
+	data, _ := ioutil.ReadAll(resp.Body)
+	var dat IbmJson
+	if err := json.Unmarshal(data, &dat); err != nil {
+		panic(err)
+	}
+	i := Company{Company: "IBM", Elapsed: elapsed.Seconds()}
+	for _, tag := range dat.Images[0].Classifiers[0].Tags {
+		i.Tags = append(i.Tags, Tag{tag.Label, tag.Score})
+	}
 
-  return &m
+	return &i
 }
---
+++ static/js/tweaks.js
@@ -0,0 +1,62 @@
+//Tweaks for the goptometry module by Eamon Collins
+
+//map of competitors equivalent models
+var model_map = {
+  "Google": {
+    "aaa03c23b3724a16a56b629203edc62c" : "General", //general
+    "eee28c313d69466f836ab83287a54ed9" : "General", //Travel
+    "bd367be194cf45149e75f01d59f77ba7" : "General", //Food
+    "e9576d86d2004ed1a38ba0cf39ecb4b1" : "NSFW",    //NSFW
+    "e466caa0619f444ab97497640cefc4dc" : "General",  //Celeb
+    "e0be3b9d6a454f0493ac3a30784001ff" : "General",  //apparel
+    "c0c0ac362b03416da06ab3fa36fb58e3" : "General", //demographics
+    "c443119bf2ed4da98487520d01a0b1e3" : "Logo",    //Logo
+    "a403429f2ddf4b49b307e318f00e528b" : "Face Detect", //Face detect
+  },
+  "Microsoft": {
+    "aaa03c23b3724a16a56b629203edc62c" : "General", //general
+    "eee28c313d69466f836ab83287a54ed9" : "General", //Travel
+    "bd367be194cf45149e75f01d59f77ba7" : "General", //Food
+    "e9576d86d2004ed1a38ba0cf39ecb4b1" : "NSFW",    //NSFW
+    "e466caa0619f444ab97497640cefc4dc" : "Celebrity",  //Celeb
+    "e0be3b9d6a454f0493ac3a30784001ff" : "General",  //apparel
+    "c0c0ac362b03416da06ab3fa36fb58e3" : "General", //demographics
+    "c443119bf2ed4da98487520d01a0b1e3" : "General",    //Logo
+    "a403429f2ddf4b49b307e318f00e528b" : "Face Detect", //Face detect
+  },
+  "Amazon": {
+    "aaa03c23b3724a16a56b629203edc62c" : "General", //general
+    "eee28c313d69466f836ab83287a54ed9" : "General", //Travel
+    "bd367be194cf45149e75f01d59f77ba7" : "General", //Food
+    "e9576d86d2004ed1a38ba0cf39ecb4b1" : "NSFW",    //NSFW
+    "e466caa0619f444ab97497640cefc4dc" : "Celebrity",  //Celeb
+    "e0be3b9d6a454f0493ac3a30784001ff" : "General",  //apparel
+    "c0c0ac362b03416da06ab3fa36fb58e3" : "General", //demographics
+    "c443119bf2ed4da98487520d01a0b1e3" : "General",    //Logo
+    "a403429f2ddf4b49b307e318f00e528b" : "Face Detect", //Face detect
+  },
+  "IBM": {
+    "aaa03c23b3724a16a56b629203edc62c" : "General", //general
+    "eee28c313d69466f836ab83287a54ed9" : "General", //Travel
+    "bd367be194cf45149e75f01d59f77ba7" : "General", //Food
+    "e9576d86d2004ed1a38ba0cf39ecb4b1" : "General",    //NSFW
+    "e466caa0619f444ab97497640cefc4dc" : "General",  //Celeb
+    "e0be3b9d6a454f0493ac3a30784001ff" : "General",  //apparel
+    "c0c0ac362b03416da06ab3fa36fb58e3" : "General", //demographics
+    "c443119bf2ed4da98487520d01a0b1e3" : "General",    //Logo
+    "a403429f2ddf4b49b307e318f00e528b" : "Face Detect", //Face detect
+  },
+}
+
+$(document).ready(function() {
+
+  //When a clarifai model is selected, change the text boxes for each competitor to reflect their equivalent model/function
+  $("#model_id").change(function(){
+    $("#competitor-fieldset .comp-model-text").each(function(){
+      $("this").prevAll().each(function(){console.log(this)})
+      $("this").val(model_map[$("this").prev("label input").val()][$("#model_id").val()])
+    })
+  })
+
+
+})
